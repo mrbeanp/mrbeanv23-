dc25d7eeebb8207de6a6100d6a05728a03bb0477
mrbeanp <100376487+mrbeanp@users.noreply.github.com>
2022-07-16 22:50:04 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -156,7 +156,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
             [
                 InlineKeyboardButton(
                     text=f"{imdb.get('title')} - {imdb.get('year')}",
-                    url=imdb['ur'],
+                    url=imdb['url'],
                 )
             ],
             [
